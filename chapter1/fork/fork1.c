37e500d9c34d3e6f73f2734e6b67ad25606c7c41
anancds <anancds@163.com>
2019-05-12 23:17:27 +0800

    added fork

---
+++ chapter1/fork/fork1.c
@@ -0,0 +1,23 @@
+#include <unistd.h>  
+#include <stdio.h>   
+
+// https://www.cnblogs.com/hoobey/p/6813220.html
+
+int main ()   
+{   
+    pid_t fpid; //fpid表示fork函数返回的值  
+    int count=0;  
+    fpid=fork();   
+    if (fpid < 0)   
+        printf("error in fork!");   
+    else if (fpid == 0) {  
+        printf("i am the child process, my process id is %d\n",getpid());   
+        count++;  
+    }  
+    else {  
+        printf("i am the parent process, my process id is %d\n",getpid());   
+        count++;  
+    }  
+    printf("统计结果是: %d\n",count);  
+    return 0;  
+} 
